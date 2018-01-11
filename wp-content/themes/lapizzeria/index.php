35fe730a7e8ba764d9076aa467f1ec83ed403541
Hernan Torres <hetorresp@gmail.com>
2018-01-11 18:08:29 -0500

    011118-18:08

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,9 +1,14 @@
-<?php
-    while(have_posts()):the_post();
-        the_title();
-
-        the_content();
-
-    endwhile
-
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Hola</h1>
+    
+</body>
+</html>
