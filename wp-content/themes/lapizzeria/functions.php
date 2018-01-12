0a8c2c12fdef01ed1cb65520d1d9ce5a924d6c5b
Hernan Torres <hetorresp@gmail.com>
2018-01-12 14:52:51 -0500

    120118 14:52

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -1,6 +1,9 @@
 <?php
     function lapizzeria_styles() {
-        wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+        wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '7.0.0' );
+        wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0' );
+        wp_enqueue_style('normalize');
+        wp_enqueue_style('style');
 
     }
 
