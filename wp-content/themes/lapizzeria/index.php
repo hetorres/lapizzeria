c733039fa726334163dc8bf942fa0f44ea38e601
Hernan Torres <hetorresp@gmail.com>
2018-02-05 11:36:06 -0500

    050218 11:35

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,5 +1,56 @@
 <?php get_header(); ?>
 
-    <h1>Hola desde index.php</h1>
+    <?php
+        $pagina_blog = get_option('page_for_posts');
+        $imagen = get_post_thumbnail_id($pagina_blog);
+        $imagen = wp_get_attachment_image_src( $imagen, 'full' );
+    
+    ?>
 
-<?php get_footer(); ?>
+
+        <div class="hero" style="background-image:url(<?php echo $imagen[0]; ?>">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                    <h1><?php echo get_the_title( $pagina_blog ); ?></h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="principal contenedor">
+            <div class="contenedor-grid">
+                <main class="columnas2-3 contenido-paginas">
+
+                    <?php while(have_posts()): the_post(); ?>
+                        <article class="entrada-blog">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail( 'especialidades' ); ?>
+                            </a>
+                        
+                            <header class="informacion-entrada clear">
+                                <div class="fecha">
+                                    <time>
+                                        <?php echo the_time('d'); ?>
+                                        <span><?php the_time('M'); ?></span>
+                                    </time>
+                                </div>
+                                <div class="titulo-entrada">
+                                    <?php the_title('<h2>', '</h2>'); ?>
+
+                                    <p class="autor">
+                                        <i class="fa fa-user" aria-hiden="true"> <?php the_author(); ?></i>
+                                    </p>
+                                </div>
+
+                            </header>
+                            <div class="contenido-entrada">
+                                <?php the_excerpt(); ?>
+                                <a href="<?php the_permalink(); ?>" class="button rojo">Leer más</a>
+                            </div>
+                        </article>
+                    <?php endwhile; ?>
+                </main>
+            </div><!-- .contenedor grid -->
+        </div><!-- principal -->
+
+
+<?php get_footer(  ); ?>
