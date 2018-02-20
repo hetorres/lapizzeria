f1a3f6d29ef8471014a7e1d977a582cfc9737f0d
Hernan Torres <hetorresp@gmail.com>
2018-02-20 10:18:29 -0500

    200218 10:18

--- wp-content/themes/lapizzeria/inc/opciones.php
+++ wp-content/themes/lapizzeria/inc/opciones.php
@@ -16,7 +16,34 @@ function lapizzeria_reservaciones(){
     ?>
         <div class="wrap">
             <h1>Reservaciones</h1>
-        
+
+            <table class="wp-list-table widefat striped">
+                <thead>
+                    <tr>
+                        <th class="manage-column">ID</th>
+                        <th class="manage-column">Nombre</th>
+                        <th class="manage-column">Fecha de Reserva</th>
+                        <th class="manage-column">Correo</th>
+                        <th class="manage-column">Teléfono</th>
+                        <th class="manage-column">Mensaje</th>
+                    </tr>
+                </thead>
+                <body>
+                    <?php global $wpdb;
+                        $reservaciones = $wpdb->prefix . 'reservaciones';
+                        $registros = $wpdb->get_results("SELECT * FROM $reservaciones", ARRAY_A);
+                        foreach($registros as $registro){ ?>
+                              <tr>
+                                <td><?php echo $registro['id']; ?> </td>
+                                <td><?php echo $registro['nombre']; ?> </td>
+                                <td><?php echo $registro['fecha']; ?> </td>
+                                <td><?php echo $registro['correo']; ?> </td>
+                                <td><?php echo $registro['telefono']; ?> </td>
+                                <td><?php echo $registro['mensaje']; ?> </td>
+                              </tr>
+                        <?php }  ?>
+                </body>
+            </table>
         </div>
     <?php
 }
